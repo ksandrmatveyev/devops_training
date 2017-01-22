684ad1cc0dece9b851fb4578621b9dae278b4ead
Aleksandr Matveyev <ksandr.matveyev@gmail.com>
2017-01-22 22:55:45 +0300

    Edit JKMount

--- edit_httpdconf.sh
+++ edit_httpdconf.sh
@@ -4,4 +4,4 @@ sudo echo "JkWorkersFile conf/workers.properties" >> /etc/httpd/conf/httpd.conf
 sudo echo "JkShmFile /tmp/shm" >> /etc/httpd/conf/httpd.conf
 sudo echo "JkLogFile logs/mod_jk.log" >> /etc/httpd/conf/httpd.conf
 sudo echo "JkLogLevel info" >> /etc/httpd/conf/httpd.conf
-sudo echo "JkMount /test* lb" >> /etc/httpd/conf/httpd.conf
+sudo echo "JkMount /testapp* lb" >> /etc/httpd/conf/httpd.conf
